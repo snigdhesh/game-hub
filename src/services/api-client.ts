5a822f9506b0b3a5856ac5f71ebcc66396e032ee
naga vadlapudi <nagain2017@outlook.com>
2024-04-01 16:21:15 -0400

    Fetched the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//This is the API client we are creating using create() method, and we can use this client to make all HTTP requests.
+//With this configuration, this key will be included in query string of every request, we send to backend, via axios
+const apiClient = axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params:{
+        key: '0c8bec16e9e241cca20958d0a2ef7ab9'
+    }
+})
+
+export default apiClient;
