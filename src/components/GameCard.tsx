235a4ceee73e7d931061794313b26607905d7cf8
snigdhesh <snigdhesh@live.com>
2024-04-02 12:19:41 -0400

    Displayed platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,14 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
+import PlatformIconList from "./PlatformIconList"
 
 interface GameCardProps{
     game: Game
+}
+
+{/* With below mapping, we are creating an array of platform objects */}
+const getPlatforms = (game: Game) =>{
+  return game.parent_platforms.map(p=>p.platform)
 }
 
 export default function GameCard({game}:GameCardProps) {
@@ -11,6 +17,7 @@ export default function GameCard({game}:GameCardProps) {
           <Image src={game.background_image} />
           <CardBody>
             <Heading fontSize="xl">{game.name}</Heading>
+            <PlatformIconList platforms={getPlatforms(game)}/>
           </CardBody>
         </Card>
   )
