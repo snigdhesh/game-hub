971a47acbfc1ea921cce3bd0668ebfcb6d3cda7e
snigdhesh <snigdhesh@live.com>
2024-04-02 13:11:43 -0400

    Added score badges to game cards

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,14 @@
+import { Badge } from '@chakra-ui/react'
+
+interface Props {
+    score: number
+}
+
+export default function CriticScore({ score }: Props) {
+    let color = score > 70 ? 'green' : score > 60 ? 'yellow' : '';
+    return (
+        <>
+            <Badge colorScheme={color} fontSize={15} paddingX={3} borderRadius={5}>{score}</Badge>
+        </>
+    )
+}
