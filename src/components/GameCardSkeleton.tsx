9d1f5b36f22f79bc95da30b35ce25c875c769161
snigdhesh <snigdhesh@live.com>
2024-04-02 14:12:51 -0400

    Loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, HStack, Heading, Image, Skeleton, SkeletonText } from '@chakra-ui/react'
+
+
+export default function GameCardSkeleton() {
+    return (
+        <>
+            <Card width={300} height={300} margin={5} overflow="hidden">
+                <CardBody>
+                    <Image />
+                    <SkeletonText />
+                </CardBody>
+            </Card>
+        </>
+    )
+}
