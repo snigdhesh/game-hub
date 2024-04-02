1e3a9307a2fad7968ad13d967e439fd54193747f
snigdhesh <snigdhesh@live.com>
2024-04-02 13:11:43 -0400

    Added score badges to game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,24 +1,28 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react"
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
-interface GameCardProps{
-    game: Game
+interface GameCardProps {
+  game: Game
 }
 
-{/* With below mapping, we are creating an array of platform objects */}
-const getPlatforms = (game: Game) =>{
-  return game.parent_platforms.map(p=>p.platform)
+{/* With below mapping, we are creating an array of platform objects */ }
+const getPlatforms = (game: Game) => {
+  return game.parent_platforms.map(p => p.platform)
 }
 
-export default function GameCard({game}:GameCardProps) {
+export default function GameCard({ game }: GameCardProps) {
   return (
     <Card key={game.id} margin={5} overflow="hidden">
-          <Image src={game.background_image} />
-          <CardBody>
-            <Heading fontSize="xl">{game.name}</Heading>
-            <PlatformIconList platforms={getPlatforms(game)}/>
-          </CardBody>
-        </Card>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList platforms={getPlatforms(game)} />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
   )
 }
