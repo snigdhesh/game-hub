c466f2429543e29403b98ea34f680fe3eeae3f24
snigdhesh <snigdhesh@live.com>
2024-04-19 18:49:26 -0400

    Build Expandable Text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@chakra-ui/react';
+import React, { ReactNode, useState } from 'react'
+
+interface Props {
+  text: string;
+}
+
+const ExpandableText = ({ text }: Props) => {
+
+  const [expandable, setExpandable] = useState(false);
+
+  const summary = expandable ? text.substring(0, 100) + "... " : text;
+
+  return (
+    <div>{summary} <Button size="xs" colorScheme='yellow' onClick={() => setExpandable(!expandable)}>{expandable ? 'Show more' : 'Show less'}</Button></div>
+  )
+}
+
+export default ExpandableText
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,17 +1,22 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Button, Heading, Spinner, Text, useStatStyles } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
+import { useEffect, useState } from 'react';
+import ExpandableText from '../components/ExpandableText';
 
 const GameDetailPage = () => {
     const { slug } = useParams();
     const { data: game, error, isLoading } = useGame(slug!)
+    const [description, setDescription] = useState();
 
     if (isLoading) return <Spinner />
     if (error || !game) throw error;
+
+
     return (
         <>
             <Heading>{game.name}</Heading>
-            <Text>{game.description_raw}</Text>
+            <ExpandableText text={game.description_raw}/>
         </>
     )
 }
